4989893dd55d0bec98d75f5c7c457900e9f9eae0
Abhishek-shankar <developer2@ideausher.com>
2025-03-07 14:15:19 +0530

    AS:secured

--- app/firebase.js
+++ app/firebase.js
@@ -7,15 +7,15 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseConfig = {
-  apiKey: "AIzaSyAXeGMu_6V2bpNKoCONeyGDzmXnnIYuBJY",
-  authDomain: "login-with-github-ed0b8.firebaseapp.com",
-  projectId: "login-with-github-ed0b8",
-  storageBucket: "login-with-github-ed0b8.firebasestorage.app",
-  messagingSenderId: "454909506265",
-  appId: "1:454909506265:web:7819e389817acfeab844c8",
-  measurementId: "G-SBC8X770JT"
+    apiKey: process.env.NEXT_PUBLIC_API_KEY,
+    authDomain: "login-with-github-ed0b8.firebaseapp.com",
+    projectId: "login-with-github-ed0b8",
+    storageBucket: "login-with-github-ed0b8.firebasestorage.app",
+    messagingSenderId: "454909506265",
+    appId: "1:454909506265:web:7819e389817acfeab844c8",
+    measurementId: "G-SBC8X770JT"
 };
 
 // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 //  const analytics = getAnalytics(app);
